817d77a08da9813d1cf2383356a0f29d45f1204a
Alina Cosor <92606282+Cosor13@users.noreply.github.com>
2022-06-16 10:26:48 -0500

    Update VPCvpc.tf

--- VPCvpc.tf
+++ VPCvpc.tf
@@ -1,4 +1,14 @@
-provider "google" {
-  project     = "team3-project-340215"
-  region      = "us-central1"
+resource "google_compute_network" "vpc" {
+	name = "globalvpc"
+	auto_create_subnetworks = "true"
+	routing_mode = "GLOBAL"
+}
+
+variable "vpc_config" {
+	type = map(any)
+	default = {
+		vpc_name = "vpc"
+		region = "us-central1"
+		zone = "us-central1-c"
+	}
 }
